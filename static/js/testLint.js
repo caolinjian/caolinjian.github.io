7426131ec2a799c03ef71c6ef447d6bbad859b0a
phoenix <phoenix@zenzet.com>
2016-04-27 22:50:44 +0800

    cube

---
+++ static/js/testLint.js
@@ -0,0 +1,15 @@
+'use strict';
+
+console.log('Hello world!');
+
+var a = 1;
+var b = {
+    'a': 2,
+    'abd_dsa': 4
+};
+
+function ads() {
+    var c = 2;
+    return c + a + b.a;
+}
+console.log(ads())
